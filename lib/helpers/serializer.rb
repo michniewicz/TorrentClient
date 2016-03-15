5ebd7d48afaa5f3b694ed907661fd7b5b24c1f88
Kostya <eldyrin@gmail.com>
2016-03-15 01:26:31 +0100

    missing space

--- lib/helpers/serializer.rb
+++ lib/helpers/serializer.rb
@@ -1,7 +1,11 @@
 require 'json'
+##
+# Represents Serializer module
+# helper module for files serialize/deserialize
+#
 module Serializer
   def self.serialize(file_name, object)
-    File.open(file_name, 'w') {|f| f.write(object.to_json) }
+    File.open(file_name, 'w') { |f| f.write(object.to_json) }
   end
 
   def self.deserialize(file_name)
